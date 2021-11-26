625cab8f9d4a780ebb908b514df62e339a4178a4
Kosala Yapa <kosalayapa@gmail.com>
2021-11-26 17:28:54 +0800

    Changes invite component name

--- src/app/interceptors/loading-interceptor.ts
+++ src/app/interceptors/loading-interceptor.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpResponse,HttpRequest, HttpHandler, HttpEvent,HttpInterceptor} from '@angular/common/http';
+import { HttpResponse, HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LoadingService } from '../services/loading.service';
-import {finalize} from 'rxjs/operators';
+import { finalize } from 'rxjs/operators';
 
 /** A HTTP interceptor for showing/hiding a loading indicator */
 @Injectable()
@@ -12,10 +12,10 @@ export class LoaderInterceptor implements HttpInterceptor {
   constructor(private loaderService: LoadingService) { }
 
   intercept(req: HttpRequest<{}>, next: HttpHandler): Observable<HttpEvent<{}>> {
- 
+
     this.loaderService.show();
-    return next.handle(req).pipe(finalize(()=> {
-        this.loaderService.hide();
+    return next.handle(req).pipe(finalize(() => {
+      this.loaderService.hide();
     }));
   }
 }
