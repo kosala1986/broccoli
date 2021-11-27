86faf01f25c93a61f720d4b389c30657be44dd89
Kosala Yapa <kosalayapa@gmail.com>
2021-11-27 21:50:26 +0800

    Adds unit test cases for user service

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -2,12 +2,16 @@ import { TestBed } from '@angular/core/testing';
 import { HttpService } from './http.service';
 import { UserService } from './user.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+import { User } from '../shared/models/user';
 import { of } from 'rxjs';
 
 describe('UserService', () => {
   let service: UserService;
   let mockHttpService: HttpService;
   let httpTestingController: HttpTestingController;
+
+  const mockUser: User = new User();
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -16,7 +20,23 @@ describe('UserService', () => {
     service = TestBed.inject(UserService);
     mockHttpService = TestBed.inject(HttpService);
     httpTestingController = TestBed.inject(HttpTestingController);
-
   });
- 
+
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('returns data when inviteUser() is successful', () => {
+    service.inviteUser(mockUser).subscribe(response => {
+      expect(response).toEqual('Registered');
+    });
+
+    const req = httpTestingController.expectOne(environment.inviteUrl);
+    expect(req.request.method).toBe("POST");
+    req.flush('Registered');
+
+    httpTestingController.verify();
+  });
+
 });
